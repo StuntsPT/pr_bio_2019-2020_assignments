300fc6c9aab6a94c8686816fb1c8798bf1374946
Ricado Santos <paladino112@gmail.com>
2020-01-22 16:41:23 +0000

    Alteração por command line dos ficheiros

--- reference_mapping/Ricardo_Santos_commands.sh
+++ reference_mapping/Ricardo_Santos_commands.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-samtools depth assembly.bam | grep "3016" -nm1 |cut -f3 > Ricardo_Santos_results_Persephone.txt
+samtools depth assembly.bam | grep "3016" -nm1 |cut -f3 > Ricardo_Santos_results.txt
